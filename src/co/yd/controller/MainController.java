df20b893835fec9bb131b7e02d672e628d1f4ce3
jiwonnie0410 <jiwonnie0410@gmail.com>
2019-09-26 16:16:05 +0900

    16:15 지원 maincontroller 수정 커밋

--- src/co/yd/controller/MainController.java
+++ src/co/yd/controller/MainController.java
@@ -3,7 +3,6 @@ package co.yd.controller;
 import java.io.IOException;
 import java.util.HashMap;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,43 +13,48 @@ import javax.servlet.http.HttpServletResponse;
 import co.yd.command.Command;
 import co.yd.command.IndexCommand;
 
-/**
- * Servlet implementation class MainController
- */
 @WebServlet("/MainController")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private HashMap<String, Command> map = null;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainController() {
-        super();
-    }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-	map = new HashMap<String, Command>();
-	map.put("/index.do", new IndexCommand());
-	
+	public MainController() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void init(ServletConfig config) throws ServletException {
+		map = new HashMap<String, Command>();
+		map.put("/index.do", new IndexCommand());
+		// 수림 추가
+		
+		
+		
+		
+		
+		// 연우 추가
+		
+		
+		
+		
+		
+		// 지원 추가
+		
+		
+		
+		
+		
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 들어온 요청을 확인 후 실행시켜 주는 부분
 		request.setCharacterEncoding("utf-8");
-		String uri=request.getRequestURI();
-		String context=request.getContextPath();
-		String path=uri.substring(context.length());
-		
-		Command comm=map.get(path);
+		String uri = request.getRequestURI();
+		String context = request.getContextPath();
+		String path = uri.substring(context.length());
+
+		Command comm = map.get(path);
 		comm.execute(request, response);
-	
 	}
 
 }
