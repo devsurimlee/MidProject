b61a56f23d071086001275346b92c56c89cbc2be
jiwonnie0410 <jiwonnie0410@gmail.com>
2019-10-01 12:34:08 +0900

    지원 beanutil

--- src/co/yd/command/InsertMemberCommand.java
+++ src/co/yd/command/InsertMemberCommand.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import co.yd.dao.MemberDAO;
 import co.yd.dto.MemberDTO;
 
@@ -16,15 +18,23 @@ public class InsertMemberCommand implements Command {
 		MemberDTO dto = new MemberDTO();
 		MemberDAO dao = new MemberDAO();
 		String path = null;
+		
+		try {
+			BeanUtils.copyProperties(dto, request.getParameterMap()); // 하나씩 안 넣고 전체를 한번에 넣어줌
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		dto.setmId(request.getParameter("id"));
-		dto.setmPw(request.getParameter("password"));
-		dto.setmName(request.getParameter("name"));
-		dto.setmEmail(request.getParameter("email"));
-		dto.setmPostcode(request.getParameter("postcode"));
-		dto.setmAddress1(request.getParameter("address1"));
-		dto.setmAddress2(request.getParameter("address2"));
-		dto.setmPhone(request.getParameter("phone"));
+		/*
+		 * dto.setmId(request.getParameter("id"));
+		 * dto.setmPw(request.getParameter("password"));
+		 * dto.setmName(request.getParameter("name"));
+		 * dto.setmEmail(request.getParameter("email"));
+		 * dto.setmPostcode(request.getParameter("postcode"));
+		 * dto.setmAddress1(request.getParameter("address1"));
+		 * dto.setmAddress2(request.getParameter("address2"));
+		 * dto.setmPhone(request.getParameter("phone"));
+		 */
 
 		System.out.println(request.getParameter("id"));
 		
