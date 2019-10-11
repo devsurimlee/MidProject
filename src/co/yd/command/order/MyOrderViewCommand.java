cec242bda5f68648f606e7c613ae6b3348219099
devsurimlee <devsurimlee@gmail.com>
2019-10-11 16:30:29 +0900

    오더즈 필터링중

--- src/co/yd/command/order/MyOrderViewCommand.java
+++ src/co/yd/command/order/MyOrderViewCommand.java
@@ -8,7 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.catalina.connector.Request;
+
 import co.yd.command.Command;
+import co.yd.dao.AdminOrderDAO;
 import co.yd.dao.OrderDAO;
 import co.yd.dto.OrderDTO;
 
@@ -17,18 +20,36 @@ public class MyOrderViewCommand implements Command{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
 		HttpSession session = request.getSession(false);
-
+		String id =  (String)session.getAttribute("id");
 		
-		ArrayList<OrderDTO> list = new ArrayList<OrderDTO>();
+		AdminOrderDAO dao = new AdminOrderDAO();
 		OrderDTO dto = new OrderDTO();
-		OrderDAO dao = new OrderDAO();
-		
-		dto.setmId((String)session.getAttribute("id")); 
-		dao.selectMyOrders(list);
-		
-		
+		String p = request.getParameter("p"); // 페이지번호
+		int pageNo = 1;
+		if (p != null && !p.isEmpty()) {
+			pageNo = Integer.parseInt(p);
+		}
+		int first, last; 		// 조회할 시작과 끝 레코드 번호
+		int recordTotal; 		// 총레코드 갯수(DB조회)
+		int pagePerRecord = 5;  // 한페이지에 출력할 레코드 건수
+		int pageCnt; 			// 페이지수
+		first = (pageNo - 1) * pagePerRecord + 1; // 해당페이지의 시작레코드
+		last = first + pagePerRecord - 1; // 해당페이지의 마지막레코드
+		String status = null;
+		recordTotal = dao.countOrders(status); // 전체레코드 수
+		pageCnt = recordTotal / pagePerRecord + (recordTotal % pagePerRecord > 0 ? 1 : 0);// 마지막페이지번호
+
+		try {
+			request.setAttribute("orderList", dao.selectAll(dto, first, last));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("pageCnt", pageCnt);
+		// 뷰페이지로 포워드
 		return "order_jsp/myOrderView.jsp";
+		
 	}
 
 }
